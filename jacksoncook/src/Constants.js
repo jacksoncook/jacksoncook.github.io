3b5f3ba2546a4fbbbc173d3b5ff2573c4b8743bc
jacksoncook <jacksoncook73@gmail.com>
2023-10-16 21:23:49 -0700

    Adding lyrics and styling for modal

--- jacksoncook/src/Constants.js
+++ jacksoncook/src/Constants.js
@@ -5,6 +5,7 @@ import skeleton from './images/album_cover_skeleton.jpg';
 import ocarina_dude from './images/album_cover_ocarina_dude.jpg';
 import bitchin_hour from './images/album_cover_bitchin_hour.jpg';
 import lost from './images/album_cover_lost.jpg';
+import text from './lyrics/OcarinaDude.txt';
 
 export const TENNIS_VIDEOS = [
   {   
@@ -55,31 +56,41 @@ export const WRITTEN_PIECES = [
   },
 ]
 
+const OCARINA_DUDE_LYRICS = "~Jacksonian~\n\nYoung professional yuppie\nPet yo' puppy\nHit the gym everyday with my duffie\n\nI should feel lucky\nTo have money\nBut I don’t\nI am ungrateful fuck me\n\nAlways hungry\nCan’t feel comfy\nChasing dreams like cumulus so puffy\n\nHaters can’t snuff me\nPolice don’t cuff me\nPlay my ocarina so my music can drug me\n\nEvery day trying to improve\nThat is my signature move\nReadin’, skiiin’, trainin’, playin’, workin’ on communication\n\nWaitin’ for that day\nOn my bed I lay\nWhat I’m not doing my thoughts don’t say\n\nDistract me, attack me, you will never crack me\nI am a man with a plan outran the lands of painting tomato cans\n\nManufacturing the happenings avoiding my own saddening\nEverytime I start unravelling I keep on paddling\nIn my mind these thoughts keep rattling\nRattatattatattatatattling"
+
+const ROTI_LYRICS = "~Jacksonian~\n\nHello from Hawaii\nAmongst the chickens!\n\nSun goes up\nSun goes down\nThat’s called the sunrise and sunset\n\nBall of fire\nNever tires (never tires)\nNever ending sunrise and sunset\n\nTide comes in\nTide goes out\nThat’s the motion of the ocean\n\nAlways emotional\nComes the pull\nThat’s the motion of the ocean\n\nMeet new friends\nLearn about\nThat’s the rhythm of the island\n\nFeelings come\nFeelings go\nThat’s the rhythm of the I (chicken)"
+
+const FOTI_LYRICS = "~Jacksonian~\n\nDrivin’ down to Tulsa,\nConnor and George in soe\nChasing our dreams,\ncarrying youthful glow\nMet Connor on the playground,\nboth of us two years old\nBound by destiny,\nto travels yet been told\nMet George in the hallway,\nyoung minds yet to mold\nCollege dorm-mates,\nluck of the dice rolled.\n\nCows to our left,\ncorn to our right\nRollin’ in the hatchback,\nwe ain’t got no plight\nWhat waits for us in Tulsa,\nwe have no foresight\nFor love…we continue to fight\n\nRiding across the country,\nyears to catch up on\nSharing bout past lovers,\ncrackin’ some cold ones\nSharing success\nSharing failures\nBeing truthful and honest\nSharing in prayer\nNot much has changed\nafter all these years\nBeautiful stars to see\nafter the clouds clear\n\nGeorge has a masters\nJackson is a rapper\nConnor still dresses dapper\nBut the question at hand\nHas yet to land\nIs Connor’s E  girl real?"
+
+const MR_CHEWFLOCKA_LYRICS = "~Jacksonian~\n\nMr. Chewflocka?\nGLRGLRGLRGLR\n\nGLRGLRGLRGLR\n\nGLRGLRGLRGLR\n\nMr. Chewflocka?\n\nGLRGLRGLRGLR\n\nMr. Chewflocka?\n\nGLRGLRGLRGLR"
+
+const SKELETON_LYRICS = "~Jacksonian~\n\nI am a skeleton\nThat is irrelevant\n\nInfrastructure\nCore\nGroundwork.\nIrrigation\nFoundation\nBones\n\nSkeleton provide support for everything you do\nBones muscle tissue\n\nThey call a skeleton head a skull\n\nCrankin’ my hog walkin’ my dog\nPee on a fire hydrant\nThat’s me, not my dog\nI pee on anything that moves and doesn’t\nCuz sometimes I gotta pee\n\nOsteopotic yeh Im a little neurotic\nBrittle stone quakes alone til the point were the joint breaks\nA mineral fate inundated by lactate from frustrate you cupcake\nRiddling as you’re fiddling tid tad twiddling\nwillowing as the tick tock ticketing ricketing ricketing ricketing. Fidgeting as the smoke starts billowing.\nAbandon your vessel or wrestle perpetual ancestral spectral pretzels\nNuzzled in each bezel\nlevel shoveling deeper and deeper til you’re befuddled forever uncuddled always troubled\n\nRespect your core"
+
 export const SONG_LYRICS = [
   {
     "title": "Ocarina Dude",
     "album_cover_path": ocarina_dude,
-    "lyrics": "",
+    "lyrics": OCARINA_DUDE_LYRICS,
   },
   {
     "title": "Rhythm of the Island",
     "album_cover_path": roti,
-    "lyrics": "",
+    "lyrics": ROTI_LYRICS,
   },
   {
     "title": "Friends of the Past",
     "album_cover_path": foti,
-    "lyrics": "",
+    "lyrics": FOTI_LYRICS,
   },
   {
     "title": "Mr. Chewflocka",
     "album_cover_path": chewflocka,
-    "lyrics": "",
+    "lyrics": MR_CHEWFLOCKA_LYRICS,
   },
   {
     "title": "Skeleton",
     "album_cover_path": skeleton,
-    "lyrics": "",
+    "lyrics": SKELETON_LYRICS,
   },
   {
     "title": "Bitchin' Hour",
@@ -92,3 +103,5 @@ export const SONG_LYRICS = [
     "lyrics": "",
   },
 ]
+
+
--- jacksoncook/src/components/Album.js
+++ jacksoncook/src/components/Album.js
@@ -14,15 +14,22 @@ export default function Album(props) {
   }
   return (
     <div className="Album-container">
-        {props.list.map((item, i) =>
-          <div id={i} className="Album-album">
-            <a href="https://github.com" onClick={(event)=>{modalItUp(event, item.title, item.lyrics)}}>
-              <img src={item.album_cover_path} className="Album-image" alt="photo_of_jackson" />
-            </a>
+      {props.list.map((item, i) =>
+        <div id={i} className="Album-album">
+          <a href="https://github.com" onClick={(event)=>{modalItUp(event, item.title, item.lyrics)}}>
+            <img src={item.album_cover_path} className="Album-image" alt="photo_of_jackson" />
+          </a>
 
-          </div>
-         )}  
-            <ReactModal id="modal" isOpen={modalVisible} onRequestClose={(event)=>{setModalVisible(false);}}>{songTitle}</ReactModal>
+        </div>
+      )}  
+      <ReactModal id="modal" isOpen={modalVisible} onRequestClose={(event)=>{setModalVisible(false);}}>
+        <p className="Album-title">
+          {songTitle}
+        </p>
+        <p className="Album-lyrics">
+          {songLyrics}
+        </p>
+      </ReactModal>
     </div>
   )
 };
